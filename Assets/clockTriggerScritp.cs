c6d89b666ff2dd6ae0930109b7d3d3f73c7fe749
TheBeardedOne <jeremy.boggs42@gmail.com>
2013-11-18 06:36:30 -0500

    CLOCK STUPIDITY

---
+++ Assets/clockTriggerScritp.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clockTriggerScritp : MonoBehaviour {
+	public GameObject clock;
+	public bool alive;
+	public AudioClip dingDong;
+	bool tripped;
+	// Use this for initialization
+	void Start () {
+		alive = true;
+		tripped = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	
+	void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Player" && alive == true){			
+	
+			if(gameObject.name == "clockTrigger" && tripped == false){
+				
+				clock.gameObject.GetComponent<clockSoundChangeScript>().change = true;
+				Debug.Log("WE HAVE ARRIVED");
+				tripped = false;
+				
+			}
+			
+			
+			//audio.Play();
+			Debug.Log("Event Triggered");
+			alive = false;
+		}
+		
+		
+    }
+}
